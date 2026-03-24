53c4356cc3ac08ac66cd6d9dce02144c5f6ea504
Dunura Witharama <dunurawitharama@gmail.com>
2026-03-24 11:05:32 +0530

    Discovery: Added HATEOAS links for self-documentation

--- src/main/java/com/smartcampus/resources/DiscoveryResource.java
+++ src/main/java/com/smartcampus/resources/DiscoveryResource.java
@@ -13,8 +13,13 @@ public class DiscoveryResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getDiscovery() {
-        Map<String, String> info = new HashMap<>();
-        info.put("version", "1.0.0");
+        Map<String, Object> info = new HashMap<>();
+        info.put("api", "Smart Campus IoT");
+        info.put("version", "v1");
+        Map<String, String> links = new HashMap<>();
+        links.put("rooms", "/api/v1/rooms");
+        links.put("sensors", "/api/v1/sensors");
+        info.put("_links", links);
         return Response.ok(info).build();
     }
 }
