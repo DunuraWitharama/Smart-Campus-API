ea172f07b6f30d7f0cc57d24ae9e81bd49a6364e
Dunura Witharama <dunurawitharama@gmail.com>
2026-03-28 13:19:44 +0530

    Rooms: Added registration logic

--- src/main/java/com/smartcampus/resources/RoomResource.java
+++ src/main/java/com/smartcampus/resources/RoomResource.java
@@ -7,14 +7,23 @@ import com.smartcampus.repository.DataStore;
 
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/rooms")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class RoomResource {
+    
     @GET
     public List<Room> getAll() { return new ArrayList<>(DataStore.rooms.values()); }
+    @POST
+    public Response create(Room room) {
+        if (DataStore.rooms.containsKey(room.getId())) return Response.status(409).build();
+        DataStore.rooms.put(room.getId(), room);
+        return Response.status(201).entity(room).build();
+    }
 }
